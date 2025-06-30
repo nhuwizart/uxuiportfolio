c17d587a7f8a0540258ffde2ab37db82a849a279
nhuwizart <nhuwizart@gmail.com>
2025-06-30 16:38:51 +0700

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -87,18 +87,110 @@ export const About = () => {
         </RevealOnScroll>
         <section id="resume" className="bg-[url(/bnr.png)] min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
-        <div className="max-w-4/5  bg-[rgba(255, 255, 255, 0.1)] backdrop-blur-lg border border-white/10 rounded-4xl shadow-sm px-25 py-12 text-center z-10 mx-auto">
-            <div className="container grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div className="col-span-2">
-                    <h1 className="text-[64px] md:text-7xl font-bold mb-6 bg-gradient-to-r from-pink-500 to-pink-400 bg-clip-text text-transparent leading-right pb-2">I'm Nhu Hoang</h1>
-                    <h1 className="text-xl md:text-xl mb-3 text-white-500">I am a <span className="text-pink-500">self-taught UX/UI Designer</span> based in Vietnam with a year of <span className="text-pink-500">experience as an e-commerce website front-end developer.</span></h1>
-                    <h1 className="text-xl md:text-xl mb-12 text-white-500">This background has provided me with valuable insights into the user experience. I'm highly passionate about UX/UI and keen to further develop my skills and career in this area.</h1>
-                    <div className="flex justify-center space-x-4">
+        <div className="max-w-4/5  bg-[rgba(255, 255, 255, 0.1)] backdrop-blur-lg border border-white/10 rounded-4xl shadow-sm px-25 py-12 text-left z-10 mx-auto">
+            <div className="container grid grid-cols-2 gap-8">
+                <div className="col-1">
+                    {/* <h1 className="text-[64px] md:text-7xl font-bold mb-6 bg-gradient-to-r from-pink-500 to-pink-400 bg-clip-text text-transparent leading-right pb-2">I'm Nhu Hoang</h1> */}
+                    <div className="edu-exp mb-10">
+                        <p className="text-4xl font-semibold mb-3 text-pink-500 drop-shadow-lg/25">Experience</p>
+                        <div className="flex mb-5">
+                            <p className="text-base text-white-500 font-semibold min-w-[150px]">2024-Present</p>
+                            <div >
+                                <p className="text-base text-white-500 uppercase font-semibold">e-commerce website front-end developer</p>
+                                <p className="text-base text-white-500">DFO Global Performance Commerce, Ho Chi minh, Viet nam</p>
+                            </div>
+                        </div>
+                        <div className="flex mb-5">
+                            <p className="text-base text-white-500 font-semibold min-w-[150px]">3/2024-5/2024</p>
+                            <div >
+                                <p className="text-base text-white-500 uppercase font-semibold">Business analyst intern</p>
+                                <p className="text-base text-white-500">KORF,  Moscow Russia</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="edu-exp ">
+                        <p className="text-4xl font-semibold mb-3 text-pink-500 drop-shadow-lg/25">Education</p>
+                        <div className="flex mb-5">
+                            <p className="text-base text-white-500 font-semibold min-w-[150px]">2020-2024</p>
+                            <div >
+                                <p className="text-base text-white-500 uppercase font-semibold">Russian Technological University</p>
+                                <p className="text-base text-white-500">bachelor of business information technology</p>
+                            </div>
+                        </div>
+                        <div className="flex mb-5">
+                            <p className="text-base text-white-500 font-semibold min-w-[150px]">2018-2019</p>
+                            <div >
+                                <p className="text-base text-white-500 uppercase font-semibold">University of Economics and Law</p>
+                                <p className="text-base text-white-500">Management Information System </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* <div className="flex justify-center space-x-4">
                         <a href="#resume" className=" text-white py-5 px-10 rounded-2xl border border-white text-xl font-semibold transition relative overflow-hidden hover:bg-pink-500 hover:border-white/0 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130,246,0.4)]">I WANT TO KNOW MORE <i class="fa-solid fa-arrow-right"></i></a>
+                    </div> */}
+                </div>
+                <div className="col-2">
+                     <div className="skills mb-10">
+                        <p className="text-4xl font-semibold mb-3 text-pink-500 drop-shadow-lg/25">Technical Skills</p>
+                        <div className="tech-skill">
+                            <div className="flex justify-between">
+                            <div className="mb-5">
+                                <p className="text-xl text-white-500 font-semibold min-w-[150px]">Design Tools</p>
+                                <div >
+                                    <div className="bg-[url(/design-icons.png)] bg-no-repeat bg-right w-[140px] h-[40px] mt-5"></div>
+                                </div>
+                            </div>
+                            <div className="mb-5">
+                                <p className="text-xl text-white-500 font-semibold min-w-[150px] capitalize">website development</p>
+                                <div >
+                                    <div className="bg-[url(/dev-icons.png)] bg-no-repeat bg-right w-[140px] h-[40px] mt-5"></div>
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="combo-skill">
+                            <div className="flex flex-wrap justify-between gap-2 mb-4">
+                            {["Research","Analyst","Website Design","App Design","UX/UI Design","Animation"].map((tech,key)=>(
+
+                                    <span  key={key} className=" text-center w-[150px] bg-pink-500/10 text-white-500 py-1 px-3 border border-white rounded-full text-base hover:shadow-[0_2px_8px_rgba(59,130,224,0.2)] transition">
+                                        {tech}
+                                    </span>
+                            ))}
+                            </div>
+                        </div>
+                    </div>
+
+                        
+                </div>
+
+                    <div className="edu-exp ">
+                        <p className="text-4xl mb-3 text-pink-500 font-semibold drop-shadow-lg/25">Language</p>
+                         <div className="tech-skill">
+                            <div className="flex justify-between">
+                            <div className="mb-5">
+                                <p className="text-xl text-white-500 font-semibold min-w-[150px]">English</p>
+                                <p className="text-base text-white-500">Pre-intermediate</p>
+
+                            </div>
+                            <div className="mb-5">
+                                <p className="text-xl text-white-500 font-semibold min-w-[150px] capitalize">Russian</p>
+                                <p className="text-base text-white-500">Intermediate</p>
+
+                            </div>
+                            <div className="mb-5">
+                                <p className="text-xl text-white-500 font-semibold min-w-[150px] capitalize">Vietnamese</p>
+                                <p className="text-base text-white-500">Native</p>
+
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
-                <div className="bg-[url(/avatar.png)] bg-no-repeat bg-right w-75 h-100 " >
-                </div>
+                {/* <div className="bg-[url(/avatar.png)] bg-no-repeat bg-right w-75 h-100 " >
+                </div> */}
             </div>
             
         </div>
